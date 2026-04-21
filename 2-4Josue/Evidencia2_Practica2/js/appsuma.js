e35ac923ef575b886619089bb3e8b3a48429c2c9
2-4Josue <josue.moraleslzr@uanl.edu.mx>
2026-04-20 18:34:52 -0600

    ev2 añadida

---
+++ 2-4Josue/Evidencia2_Practica2/js/appsuma.js
@@ -0,0 +1,33 @@
+//  crear las referencias hacia los objetos 
+// que se van a programar 
+
+const inputNum1 = document.getElementById("num1");
+const inputNum2 = document.getElementById("num2");
+const inputOperacion = document.getElementById("operacion");
+const inputResultado = document.getElementById("resultado");
+const botoncalcular = document.getElementById("botoncalcular"); 
+
+botoncalcular.addEventListener("click", e => {
+    e.preventDefault();
+
+    let num1 = parseInt(inputNum1.value);
+    let num2 = parseInt(inputNum2.value);
+    let operacion = inputOperacion.value;
+    let result = 0; 
+
+    if (operacion == "+") {
+        result = num1 + num2;
+    }
+    else if (operacion == "-") {
+        result = num1 - num2;
+    }
+    else if (operacion == "*") {
+        result = num1 * num2; // 
+    }
+    else if (operacion == "/") {
+        result = num1 / num2; // 
+    }
+
+    inputResultado.value = result;
+});
+
