aedc05cc9928ce226b387769752317c96c11abd7
monicalvt98 <monicalvtjc98@gmail.com>
2026-04-21 15:57:43 -0600

    Agregue evidencia 2

---
+++ monicalvt98/Evidencia2/js/appSuma.js
@@ -0,0 +1,27 @@
+/* Crear las referencias hacia los objetos que se van a porgramar */
+const inputNum1 = document.getElementById("num1");
+const inputNum2 = document.getElementById("num2");
+const inputOperacion = document.getElementById("operacion");
+const inputResultado = document.getElementById("resultado");
+const botonCalcular = document.getElementById("botonCalcular");
+/* Primero programar el boton porque hace una accion */
+botonCalcular.addEventListener("click", e => {
+    e.preventDefault();
+    let num1 = parseInt(inputNum1.value);
+    let num2 = parseInt(inputNum2.value);
+    let operacion = inputOperacion.value;
+    if(operacion == "+") {
+        result = num1 + num2;
+    }
+    else if(operacion == "-") {
+        result = num1 - num2;
+    }
+    else if(operacion == "*") {
+        result = num1 * num2;
+    }
+   else if(operacion = "/") {
+        result = num1 / num2;
+    }
+    inputResultado.value = result;
+});
+
